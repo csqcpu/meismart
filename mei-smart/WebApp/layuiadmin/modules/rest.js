fd43ffd1f07ca361c2e9aa2c3d42491e524412bd
csqcpu <18608900535@wo.com.cn>
2019-05-15 09:02:52 +0800

    2019-5-15

---
+++ mei-smart/WebApp/layuiadmin/modules/rest.js
@@ -0,0 +1,46 @@
+layui.define(['jquery'], function(exports){ 
+    var $ = layui.jquery;
+    var obj = {
+       ajax: function (req) {
+            $.ajax({
+                type: req.type,                
+                //url: req.url,
+                url: req.url+'?time='+(new Date()).getTime(),
+                contentType:req.contentType,
+                dataType: req.dataType,
+                headers: req.headers,
+                data: function(){
+                	
+                	var JsonRequest = "object" == typeof req.data? req.data:JSON.parse(req.data)
+                	var dataObject={}
+                	dataObject['data']=JsonRequest.data
+                	dataObject['timestamp']=layui.data(layui.setter.tableName).timestamp
+                	var encryptdata=encrypt(JSON.stringify(dataObject),layui.data(layui.setter.tableName).password);
+                	//var output={}
+                	JsonRequest['token']=layui.data(layui.setter.tableName).token
+                	JsonRequest['data']=encryptdata
+                	return JSON.stringify(JsonRequest)
+                }(),
+                success: function(res){
+                   if(res.data!=null){
+                	   var data = decrypt(res.data,layui.data(layui.setter.tableName).password)
+                	   data = JSON.parse(data)
+             		   res.data=data
+                   }
+         		   if(res.timestamp!=null){
+         	          layui.data(layui.setter.tableName, {
+         	                key:'timestamp',
+         	                value:res.timestamp
+         	              });
+         		   }
+                   req.success(res)
+                },
+                error:function(e,t){
+                	req.error(e,t)
+                }
+            });
+        }
+    };
+    //输出接口
+    exports('rest', obj);
+});
